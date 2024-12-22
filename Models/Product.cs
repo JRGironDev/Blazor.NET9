69476e775a73c7271100bc95f1e686121f60fe24
José Roberto Quiñónez Girón <joserobertoq@outlook.com>
2024-12-22 07:20:11 -0600

    Se ha añadido un nuevo enlace en el menú de navegación (`NavMenu.razor`) que apunta a una nueva página llamada "Bind Property".
    En el archivo `_Imports.razor`, se ha eliminado la importación de `Blazor.NET9.Components`.

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Blazor.NET9.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
