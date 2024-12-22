1d6583c4849ef3fbbabd60ff8782d602db18615d
José Roberto Quiñónez Girón <joserobertoq@outlook.com>
2024-12-22 07:45:56 -0600

    Añadir propiedad Price a Product y actualizar UI

--- Models/Product.cs
+++ Models/Product.cs
@@ -5,5 +5,7 @@
         public int Id { get; set; }
         public required string Name { get; set; }
         public bool IsActive { get; set; }
+
+        public double Price { get; set; }
     }
 }
